9ea1355f37c8260efdc6302e7fc0e9c4914c3e6a
rick-hayek <huangjiadong@msn.com>
2018-04-10 02:37:45 -0700

    factory method

--- src/TestApp/Program.cs
+++ src/TestApp/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DesignPattensInCSharp.SingletonPatten;
+using DesignPattensInCSharp.FactoryMethod;
 
 namespace DesignPattensInCSharp.TestApp
 {
@@ -23,8 +24,33 @@ namespace DesignPattensInCSharp.TestApp
 
             Console.WriteLine("(lazyInst1 == lazyInst2) = {0}", lazyInst1 == lazyInst2);
 
+            var dog = CreateAnimal(AnimalType.Dog);
+            dog.Move();
+
+            var bird = CreateAnimal(AnimalType.Bird);
+            bird.Move();
+
             Console.ReadLine();
+        }
+
+        static IAnimal CreateAnimal(AnimalType type)
+        {
+            Creator creator;
+            switch (type)
+            {
+                case AnimalType.Bird:
+                    creator = new BirdCreator();
+                    break;
+                case AnimalType.Dog:
+                    creator = new DogCreator();
+                    break;
+                default:
+                    creator = new DogCreator();
+                    break;
+            }
+
+            return creator.CreatAnimal();
         }
     }
 
-  }
+}
